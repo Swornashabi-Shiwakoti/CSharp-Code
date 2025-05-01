577f1ad8717adab343099a7cdaa3096631d6e040
Swornashabi <swornasabishiwakoti@gmail.com>
2025-05-01 09:58:57 -0500

    Homework1

---
+++ ReparingTheClockTower.cs
@@ -0,0 +1,24 @@
+﻿//Written by Swornashabi
+// 2/2/2025
+namespace ReparingTheClockTower
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //taking input
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+
+            //finding if the number is odd or even
+            if(number % 2 == 0)
+            {
+                Console.WriteLine("Tick");
+            }
+            else
+            {
+                Console.WriteLine("Tock");
+            }
+        }
+    }
+}
