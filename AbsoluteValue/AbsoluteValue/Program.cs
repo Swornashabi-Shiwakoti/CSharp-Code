a3bc9a9b70e177c652d58fe299a59d75994c15f9
Swornashabi-Shiwakoti <swornashabi.shiwakoti@mnsu.edu>
2025-05-01 11:01:00 -0500

    Assignment

---
+++ AbsoluteValue/AbsoluteValue/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbsoluteValue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> aList = new List<int> { 2, -1, 4, 8, 10 };
+
+            Calculator aCalculator = new Calculator();
+
+            int absSum = aCalculator.FindAbsSum(aList);
+
+            Console.WriteLine(absSum);
+        }
+    }
+}
